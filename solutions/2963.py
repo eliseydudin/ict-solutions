e952ef4180c31d67df3810f15c527a8e2d703d8d
eliseydudin <dudinelisey630@gmail.com>
2024-11-07 10:02:06 +0300

    2963 fixed

--- solutions/2963.py
+++ solutions/2963.py
@@ -1,12 +1,19 @@
-def f(n: int):
-    if n == 1:
-        return 0
-    v = f(n - 1)
-    if n % 2 == 0:
-        v = min(v, f(n // 2))
-    if n % 3 == 0:
-        v = min(v, f(n // 3))
-    return v + 1
+def solve(n: int):
+    cache = [0] * (n + 1)
+    for i in range(2, n + 1):
+        v = cache[i - 1]
+        if i % 2 == 0:
+            v = min(v, cache[i // 2])
+        if i % 3 == 0:
+            v = min(v, cache[i // 3])
+        cache[i] = v + 1
+    return cache[-1]
 
 
-print(f(int(input())))
+def main():
+    n = int(input())
+    print(solve(n))
+
+
+if __name__ == "__main__":
+    main()
