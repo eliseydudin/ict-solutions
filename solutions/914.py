b480ff9d6b97f47e029fe87451a998e7630b9ac1
eliseydudin <dudinelisey630@gmail.com>
2024-11-07 09:52:59 +0300

    removed the comment in 914

--- solutions/914.py
+++ solutions/914.py
@@ -1,22 +1,3 @@
-"""
-#include <iostream>
-int main() {
-    int n, a, b, c;
-    std::cin >> n;
-    //base
-    a = 3;
-    b = 8;
-    c = 22;
-    for (int i = 0; i < n-1; ++i) {
-        c = b * 2 + a * 2;
-        a = b;
-        b = c;
-    }
-    std::cout << a << std::endl;
-}
-"""
-
-
 def main():
     n = int(input())
     a, b, c = 3, 8, 22
