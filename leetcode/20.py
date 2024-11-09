dfc2b588c57ea22d17e1badb61e1d8c61f9d055b
eliseydudin <dudinelisey630@gmail.com>
2024-11-10 00:39:27 +0300

    leetcode 20 & better rust

---
+++ leetcode/20.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        d = {"()", "[]", "{}"}
+        for c in s:
+            if c in "({[":
+                stk.append(c)
+            elif not stk or stk.pop() + c not in d:
+                return False
+        return not stk
+
+    def __init__(self):
+        pass
+
+
+def main():
+    sol = Solution()
+    s = input()
+    print(sol.isValid(s))
+
+
+if __name__ == "__main__":
+    main()
