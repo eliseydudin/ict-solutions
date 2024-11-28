a7683a822ac40fc559a8873e5457dbf222f1c2ae
eliseydudin <dudinelisey630@gmail.com>
2024-11-28 10:11:04 +0300

    cw 28/11 task 19

--- other/cw_28.11/19/main.go
+++ other/cw_28.11/19/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+var result = make(map[int]struct{})
+
+func is_odd(num int) bool {
+	return num%2 != 0
+}
+
+func to_int(arg bool) int {
+	if arg {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func solve(num int) int {
+	if num < 100 {
+		if is_odd(num) && num != 1 {
+			result[num] = struct{}{}
+		}
+
+		solve(num + 3)
+		solve(num * 3)
+	}
+
+	return len(result)
+}
+
+func main() {
+	fmt.Println(solve(1))
 }
