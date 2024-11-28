a7885f074d33e030d1673cce301c578e0ec9b7c8
eliseydudin <dudinelisey630@gmail.com>
2024-11-28 10:36:58 +0300

    itmo 1

---
+++ other/itmo_28.11/1/main.go
@@ -0,0 +1,18 @@
+package main
+
+const N = 20
+
+func get_in_base(base int) int {
+	return base*base + base*2 + 3
+}
+
+func main() {
+	solution := 0
+	for base := 4; base < N; base++ {
+		solution += get_in_base(base)
+	}
+
+	println(solution)
+}
+
+// solution: 2872
---
+++ other/itmo_28.11/readme.md
@@ -0,0 +1,2 @@
+# itmo 28/11
+basically solutions for itmo.
