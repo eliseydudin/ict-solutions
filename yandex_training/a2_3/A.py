b5002fd8856fd3b08ff4f31d1f554ccb28873f0e
eliseydudin <dudinelisey630@gmail.com>
2025-01-09 15:50:52 +0300

    a

---
+++ yandex_training/a2_3/A.py
@@ -0,0 +1,17 @@
+def main():
+    yes_pile = set(map(str, range(1, int(input()) + 1)))
+
+    for line in iter(input, "HELP"):
+        guesses = yes_pile.intersection(line.split())
+        if len(guesses) > (len(yes_pile) - len(guesses)):
+            print("YES")
+            yes_pile = guesses
+        else:
+            print("NO")
+            yes_pile -= guesses
+
+    print(*sorted(yes_pile, key=int))
+
+
+if __name__ == "__main__":
+    main()
