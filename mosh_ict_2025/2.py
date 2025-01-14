50d39405c12185685305e294d37fc5b92e7fe987
eliseydudin <dudinelisey630@gmail.com>
2025-01-14 10:27:30 +0300

    ignore stuff + 2

--- mosh_ict_2025/2.py
+++ mosh_ict_2025/2.py
@@ -1,57 +1,75 @@
-# this code works :3
-# 70/100 points
+from bisect import bisect_right
+import math
 
 
-from functools import lru_cache
+def sieve_of_eratosthenes(limit: int):
+    is_prime = [True] * (limit + 1)
+    primes = []
+    is_prime[0] = is_prime[1] = False
+
+    for p in range(2, int(limit**0.5) + 1):
+        if is_prime[p]:
+            for i in range(p * p, limit + 1, p):
+                is_prime[i] = False
+
+    for p in range(2, limit + 1):
+        if is_prime[p]:
+            primes.append(p)
+
+    return primes
 
 
-def closest_power_of_2(num: int) -> int:
-    num -= 1
-    num |= num >> 1
-    num |= num >> 2
-    num |= num >> 4
-    num |= num >> 8
-    num |= num >> 16
-    num += 1
-    return num
-
-
-@lru_cache(None)
-def is_prime(n: int) -> bool:
-    if n <= 1:
+def is_prime(n: int, primes):
+    if n < 2:
         return False
-    if n <= 3:
-        return True
-
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
+    if n <= primes[-1]:
+        index = bisect_right(primes, n) - 1
+        return primes[index] == n
+    for prime in primes:
+        if prime * prime > n:
+            break
+        if n % prime == 0:
             return False
-        i += 6
-
     return True
 
 
-def closest_prime(num: int) -> int:
-    while not is_prime(num):
-        num += 1
-    return num
+prime_cache: dict = {}
 
 
-def solve(num: int) -> int:
-    if num == 0:
-        return 1
-    return min(closest_power_of_2(num), closest_prime(num))
+def closest_prime(n: int):
+    if n in prime_cache:
+        return prime_cache[n]
+
+    small_cache = sieve_of_eratosthenes(int(math.sqrt(10**7)) + 1)
+
+    i = n
+    while True:
+        if is_prime(i, small_cache):
+            prime_cache[n] = i
+            return i
+        i += 1
+
+
+def closest_power_of_two(n: int):
+    n -= 1
+    n |= n >> 1
+    n |= n >> 2
+    n |= n >> 4
+    n |= n >> 8
+    n |= n >> 16
+    return n + 1
 
 
 def main():
     _n = int(input())
-    nums = map(int, input().split())
-    for inp in nums:
-        print(solve(inp), end=" ")
+    inputs = map(int, input().split())
+
+    for i in inputs:
+        if i < 2:
+            print(1, end=" ")
+        else:
+            print(min(closest_power_of_two(i), closest_prime(i)), end=" ")
+
     print()
 
 
