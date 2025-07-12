686de9d670d015b5fec8c31c036ee720f8024733
eliseydudin <dudinelisey630@gmail.com>
2025-07-12 16:56:53 +0300

    111403

---
+++ informatics/111403.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+double solve_helper(double number) {
+  return number * number + std::sqrt(number);
+}
+
+int main() {
+  double c;
+  std::cin >> c;
+
+  double l = 0.0, r = std::sqrt(c);
+  while (l + std::numeric_limits<double>::epsilon() < r) {
+    double mid = (l + r) / 2.0;
+
+    if (solve_helper(mid) < c) {
+      l = mid;
+    } else {
+      r = mid;
+    }
+  }
+
+  std::cout << std::fixed << std::setprecision(9) << r << std::endl;
+  return 0;
+}
