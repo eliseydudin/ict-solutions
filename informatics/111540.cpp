20200844baf620229bc23b4137afd432596e3abf
eliseydudin <dudinelisey630@gmail.com>
2025-08-02 16:23:45 +0300

    111540 fix

--- informatics/111540.cpp
+++ informatics/111540.cpp
@@ -1,67 +1,51 @@
 // 75/100
 
 #include <iostream>
-#include <set>
 #include <vector>
 
-void dfs(size_t num, std::vector<std::vector<size_t>> &matrix,
-         std::vector<bool> &used) {
-  for (size_t i = 0; i < matrix.size(); i++) {
-    if (matrix[num][i] && !used[i]) {
-      used[i] = true;
-      dfs(i, matrix, used);
-    }
+void dfs(int vertex, std::vector<std::vector<int>> &matrix,
+         std::vector<int> &used, int group) {
+  for (auto el : matrix[vertex]) {
+    if (used[el] != -1) continue;
+    used[el] = group;
+    dfs(el, matrix, used, group);
   }
-}
-
-std::vector<bool> dfs_helper(size_t target,
-                             std::vector<std::vector<size_t>> &matrix) {
-  std::vector<bool> used(matrix.size(), false);
-  used[target] = true;
-  dfs(target, matrix, used);
-
-  return used;
 }
 
 int main() {
-  size_t n, m;
+  int n, m;
   std::cin >> n >> m;
-  std::vector<std::vector<size_t>> matrix(n, std::vector<size_t>(n));
-  std::vector<std::vector<size_t>> results;
+  std::vector<std::vector<int>> matrix(n);
 
-  std::set<size_t> to_search_for;
-  for (size_t i = 0; i < n; i++) {
-    to_search_for.insert(i);
-  }
-
-  for (size_t i = 0; i < m; i++) {
-    size_t l, r;
+  for (int i = 0; i < m; i++) {
+    int l, r;
     std::cin >> l >> r;
-    matrix[--l][--r] = 1;
-    matrix[r][l] = 1;
+    matrix[--l].push_back(--r);
+    matrix[r].push_back(l);
   }
 
-  while (!to_search_for.empty()) {
-    size_t search = *to_search_for.begin();
-    std::vector<bool> adj = dfs_helper(search, matrix);
+  int group = 0;
+  std::vector<int> used(n, -1);
+  for (int i = 0; i < n; i++) {
+    if (used[i] != -1) continue;
 
-    std::vector<size_t> res;
-    for (size_t i = 0; i < adj.size(); i++) {
-      if (!adj[i]) continue;
-      res.push_back(i);
-      to_search_for.erase(i);
-    }
-
-    results.push_back(res);
+    used[i] = group;
+    dfs(i, matrix, used, group++);
   }
 
-  std::cout << results.size() << std::endl;
+  std::vector<std::vector<int>> results(group);
+
+  std::cout << group << std::endl;
+  for (int i = 0; i < n; i++) {
+    results[used[i]].push_back(i);
+  }
+
   for (auto &res : results) {
-    std::cout << res.size() << "\n";
-    for (auto elem : res) {
-      std::cout << ++elem << " ";
+    std::cout << res.size() << '\n';
+    for (auto el : res) {
+      std::cout << ++el << ' ';
     }
-    std::cout << "\n";
+    std::cout << '\n';
   }
 
   return 0;
