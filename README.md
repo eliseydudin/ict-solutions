0b3d031487ab75703d55d43b0a03e243d3369061
eliseydudin <dudinelisey630@gmail.com>
2025-08-09 14:09:49 +0300

    runner

--- README.md
+++ README.md
@@ -3,13 +3,16 @@ solutions for tasks on different platforms ([here](https://informatics.msk.ru) f
 PS: some solutions for dynamic programming can be found [here](https://github.com/eliseydudin/dp-school)
 
 # running
-most of the solutions are written in c++, they can be compiled using meson:
-```shell
-meson setup builddir
-meson compile -C builddir
-exec builddir/<website>/<task> # for example `exec builddir/informatics/111403`
+to run the programs you are going to need:
+1. a c/c++ compiler (like `gcc` or `clang`)
+2. meson
+3. a python 3.x interpreter
+
+to run a solution use the `run.py` utility from the root of the repo, example:
+```bash
+./run.py inf 160
 ```
-all other solutions can be ran using their respective programs (like go & python)
+this will automatically compile all files & execute the program at `./informatics/160.cpp`.
 
 # contributing
 any contributions are welcome! provide any solutions for any website with programming tasks, the only requirement is that they pass _some_ tests
---
+++ run.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+
+# short forms of the possible directories
+DIRECTORY_SHORT = {
+    "acmp": "acmp",
+    "cf": "codeforces",
+    "inf": "informatics",
+    "leet": "leetcode",
+    "mosh25": "mosh_ict_2025",
+    "muni2": "municip2",
+    "timus": "timus",
+}
+
+
+# get the possible runners for a task
+def get_possible_runners(filenames: list[str]) -> set[str]:
+    runners: set[str] = set()
+
+    for filename in filenames:
+        _base, ext = os.path.splitext(filename)
+
+        match ext:
+            case "":
+                runners.add("go")
+            case ".py":
+                runners.add("python")
+            case ".cpp" | ".c":
+                runners.add("c++")
+            case _:
+                print(f"warning: unknown extension on file {filename}")
+
+    return runners
+
+
+def actually_run(dir: str, task: str, runner: str) -> int:
+    match runner:
+        case "go":
+            return os.system(f"go run ./{dir}/{task}/main.go")
+        case "python":
+            return os.system(f"python3 ./{dir}/{task}.py")
+        case "c++":
+            if not os.path.exists("builddir"):
+                os.system("meson setup builddir")
+            os.system("meson compile -C builddir")  # compile everything
+            return os.system(f"./builddir/{dir}/{task}")
+        case _:
+            pass
+
+    return 0
+
+
+# the main function, returns a number that should be the exit code of the program
+def main(
+    dir: str,
+    task: str,
+) -> int:
+    if dir not in DIRECTORY_SHORT:
+        print("error: unknown directory")
+        return 1
+
+    actual_dir = DIRECTORY_SHORT[dir]
+    possible_tasks = filter(
+        lambda x: os.path.basename(x).split(".")[0] == task,
+        os.listdir(actual_dir),
+    )
+
+    runners = get_possible_runners(list(possible_tasks))
+
+    if len(runners) == 0:
+        print("error: no runners found")
+        return 1
+    else:
+        print("found solutions in the following languages:", *runners)
+
+        if len(runners) == 1:
+            runner = runners.pop()
+        else:
+            runner = input("which one to run: ")
+            if runner not in runners:
+                print("error: invalid runner")
+                return 1
+
+    return actually_run(actual_dir, task, runner)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="run", description="Run a solution for a task in this repository"
+    )
+    parser.add_argument(
+        "dir",
+        help="The directory that contains the task",
+        choices=DIRECTORY_SHORT.keys(),
+    )
+    parser.add_argument("task", help="The task to run")
+    args = parser.parse_args()
+
+    result = main(args.dir, args.task)
+    print(
+        f"the program ended with the code {hex(result)}, {"success" if result == 0 else "failure"}"
+    )
+
+    exit(result)
