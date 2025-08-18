779b993e66344a64a9fa0af71f6ce41837143014
eliseydudin <dudinelisey630@gmail.com>
2025-08-18 12:56:28 +0300

    207

--- informatics/207.py
+++ informatics/207.py
@@ -1,55 +1,15 @@
-"""
-this code doesn't work due to the size of ints :(
-
-#include <iostream>
-#include <map>
-using i64 = unsigned long long;
-
-i64 solve(i64 n, std::map<i64, i64> &mp) {
-  if (mp.find(n) != mp.end()) {
-    return mp[n];
-  }
-
-  mp[n] = solve(n - 1, mp) + solve(n - 2, mp);
-  return mp[n];
-}
-
-i64 solve(i64 n) {
-  std::map<i64, i64> mp;
-  mp[1] = 2;
-  mp[2] = 3;
-
-  return solve(n, mp);
-}
-
-int main() {
-  std::ios::sync_with_stdio(0);
-
-  i64 n;
-  std::cin >> n;
-  std::cout << solve(n) << std::endl;
-
-  return 0;
-}
-"""
-
-
-def solve(m: int, cache: dict[int, int]) -> int:
-    data = cache.get(m)
-    if data is not None:
-        return data
-
-    data = solve(m - 1, cache) + solve(m - 2, cache)
-    cache[m] = data
-
-    return data
-
-
 def main():
     n = int(input())
-    cache = {1: 2, 2: 3}
+    cnt_ones = [0] * (n + 1)
+    cnt_zeros = [0] * (n + 1)
+    cnt_ones[0] = cnt_zeros[0] = 1
+    n -= 1
 
-    print(solve(n, cache))
+    for i in range(1, n + 1):
+        cnt_zeros[i] = cnt_zeros[i - 1] + cnt_ones[i - 1]
+        cnt_ones[i] = cnt_zeros[i - 1]
+
+    print(cnt_ones[n] + cnt_zeros[n])
 
 
 if __name__ == "__main__":
