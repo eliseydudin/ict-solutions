cb3d158993e0610a0d2289a0f3c9c36c9742bcc2
eliseydudin <dudinelisey630@gmail.com>
2025-09-13 19:01:18 +0300

    fixme 1128

---
+++ informatics/1128.cpp
@@ -0,0 +1,15 @@
+// TODO FIXME
+
+// #include <iostream>
+
+// using pos_t = std::pair<int, int>;
+
+// int main() {
+//   pos_t start, end, trap;
+//   int radius;
+
+//   std::cin >> start.first >> start.second >> end.first >> end.second >>
+//       trap.first >> trap.second >> radius;
+
+//   return 0;
+// }
