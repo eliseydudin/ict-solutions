2858eceade7d8a38230d041b2e1843224b64c424
eliseydudin <dudinelisey630@gmail.com>
2025-10-13 13:06:22 +0300

    113658

---
+++ informatics/113659.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+using map = std::map<int, std::vector<int>>;
+
+int solve(map &m, int root) {
+  int count = 1;
+
+  for (auto &child : m[root]) {
+    count += solve(m, child);
+  }
+
+  return count;
+}
+
+int main() {
+  int n, k;
+  std::cin >> n;
+
+  map input;
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    std::cin >> a >> b;
+    input[b].push_back(a);
+  }
+
+  std::cin >> k;
+  std::cout << solve(input, 0) << std::endl;
+
+  return 0;
+}
